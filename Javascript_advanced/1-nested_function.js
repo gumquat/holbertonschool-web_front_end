05519b178765568f27548b5f3c5f3ad190334720
Evan Newman-Chock <gumquat@gmail.com>
2023-11-13 13:02:54 -0600

    pushes 0-2

---
+++ Javascript_advanced/1-nested_function.js
@@ -0,0 +1,19 @@
+let globalVariable = 'Welcome';
+
+function outer() {
+  alert(globalVariable);
+  let course = "Holberton";
+
+  function inner() {
+      alert(globalVariable + " " + course);
+      let exclamation = "!";
+
+      function inception() {
+          alert(globalVariable + " " + course + " " + exclamation);
+      }
+
+      inception();
+  }
+
+  inner();
+}
